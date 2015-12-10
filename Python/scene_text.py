a88e52c9c2046ed594c103503f1dcdd9cd61d20b
sivaramanks <siva@icecreamlabs.com>
2015-12-10 12:25:18 +0530

    Tested Region Props. Boxes very small.

---
+++ Python/scene_text.py
@@ -0,0 +1,19 @@
+from skimage import measure
+from skimage import morphology
+import cv2, numpy as np
+image = cv2.imread('../test_images/o_y16.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+mserObject = cv2.MSER()
+mserRegions = mserObject.detect(gray)
+connectedComp, numOfComps = morphology.label(gray, return_num = True)
+print connectedComp, numOfComps
+mserStats = measure.regionprops(connectedComp)
+print len(mserStats)
+filtered = []
+for region in mserStats:
+    if region.eccentricity < 0.995 and region.solidity > 0.7 and region.euler_number > -4 and region.extent > 0.75:
+        x1,y1,x2,y2 = region.bbox
+        cv2.rectangle(image, (int(y1),int(x1)),(int(y2),int(x2)),255)
+        filtered.append(region)
+cv2.imwrite('../test_patches/out_text.jpg',image)
+print len(filtered)
--- Python/text_detect.py
+++ Python/text_detect.py
@@ -1,75 +1,90 @@
-import cv2
 from numpy import uint8, hstack, ndarray
-from skimage.measure import regionprops
-from skimage.morphology import skeletonize
-import skimage
-#from skimage.measure import label
-print skimage.__version__
-import skimage.measure.label as label
+import cv2
+from skimage import morphology
+from skimage import measure
+
 
 class MSER:
   def __init__(self,im):
     self.image = im
     self.gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     self.mser = cv2.MSER()
-    self.regions = mser.detect(self.gray)
+    self.regions = self.mser.detect(self.gray)
         
   def get_regions(self):
     return self.regions
         
-  def morph_skeleton(patch):
-    return skeletonize(patch).astype(uint8)*255
+  def morph_skeleton(self, patch):
+    return morphology.skeletonize(patch).astype(uint8)*255
   
-  def BG_color():
+  def BG_color(self):
     Q = hstack((self.gray[0:,], self.gray[-1:,], self.gray[:,0].flatten(),
       self.gray[:,-1].flatten()))
     return int(Q[Q==0].shape[0] <= Q[Q==1].shape[0])
   
-  def draw_patch_skel1(patch):
+  def draw_patch_skel1(self,patch):
     (_, patch) = cv2.threshold(patch, 128, 255, cv2.THRESH_OTSU)
     patch[patch == 255] = 1
     if self.BG_color(patch) == 1:
       patch = 1 - patch
     return patch
 
-  def draw_patch_skel(patch):
+  def draw_patch_skel(self, patch):
     (_,patch) = cv2.threshold(patch, 128, 255, cv2.THRESH_OTSU)
     patch[patch==255] = 1
     if self.BG_color(patch) == 1:
       patch = 1 - patch
       return self.morph_skeleton(patch)
   
-  def get_MSER_bounds():
+  def get_MSER_bounds(self):
     self.hulls = [cv2.convexHull(p.reshape(-1,1,2)) for p in self.regions]
     return self.hulls
 
-  def draw_MSER_bounds():
+  def _get_filtered_Regions_bounds(self):
+    self.f_hulls = [cv2.convexHull(p.coords) for p in
+        self.filteredRegions]
+    return self.f_hulls
+
+
+  def draw_MSER_bounds(self):
     image_dummy = self.image.copy()
     cv2.polylines(image_dummy, self.hulls, 1, (255,0,0))
+    ##cv2.polylines(image_dummy, self.f_hulls, 1, (0,255,255))
     return image_dummy
 
-  def refine_MSER():
+  def refine_MSER(self):
     h_prime = []
+    ctr = 0
     for ah in self.hulls:
-      R = cv2.boundingRectangle(ah)
+      R = cv2.boundingRect(ah)
       reg_h, reg_w = float(R[3]), float(R[2])
       a_r = reg_w/reg_h
-      if 1.2 < a_r < 0.25:
-        h_prime.append(ah)
-    
-    h_pprime = []
-    for ah in h_prime:
-      R = cv2.boundingRectangle(ah)
+      h_prime.append(ah)
       row_start, col_start = R[1], R[0]
       h, w = R[3], R[2]
       row_end, col_end = row_start+h, col_start+w
       patch = self.gray[row_start:row_end, col_start:col_end]
-      skeleton = draw_patch_skel(patch)
-      if skeleton[skeleton==255].shape[0]>10 and label(skeleton).max()<5:
-        h_pprime.append(ah)
-    return h_pprime  
+      im_patch = self.image[row_start:row_end, col_start:col_end]
+      cv2.imwrite(str(ctr)+".jpg",im_patch)
+      ctr+=1
+
+  def _filter_regions(self):
+    self.connectedComps = morphology.label(self.gray,
+        return_num = False)
+    mserStats = measure.regionprops(self.connectedComps)
+    self.filteredRegions = []
+    for region in mserStats:
+      if region.eccentricity < 0.995 and region.solidity > 0.3 and 0.2 < region.extent < 0.9 and region.euler_number > -4:
+        self.filteredRegions.append(region)
+    print len(self.filteredRegions),": Regions filtered"
+     
+  def _draw_filtered_regions(self):
+    for region in self.filteredRegions:
+      print region.convex_image
+
+
 if __name__ == "__main__":
-  img = cv2.imread('../test_images/o_y10.jpg');
+  img = cv2.imread('../test_images/o_y17.jpg');
   Obj = MSER(img)
   Obj.get_MSER_bounds()
   Obj.refine_MSER()
